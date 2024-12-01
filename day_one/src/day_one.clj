fabff3a160e162f65216f10fb1ebf3f8fe26edfe
Stephen Nelson-Smith <stephen.nelsonsmith@ticketmaster.co.uk>
2024-12-01 14:36:47 +0000

    Add: Part one solution prototype
    This is my sketch solution to what I perceive to be the problem,
    but without the actual input data.
    
    The tests need more edger cases, and the functions could probably be
    more defensive, but it's a start.

--- day_one/src/day_one.clj
+++ day_one/src/day_one.clj
@@ -1,2 +1,17 @@
 (ns day-one)
-(defn sum-of-differences [first-list second-list] 6)
+
+(defn generate-pairs 
+ [first-list second-list] 
+ (map list 
+      (sort first-list) 
+      (sort second-list)))
+
+(defn calculate-difference 
+ [pair] 
+ (Math/abs (apply - pair)))
+
+(defn sum-of-differences 
+ [first-list second-list]
+ (reduce + 
+         (map calculate-difference 
+              (generate-pairs first-list second-list))))
